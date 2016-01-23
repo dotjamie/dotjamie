21b0839506897ac64914d404a18c4a8c17ec4da4
dotjamie <dotjamie@gmail.com>
2016-01-23 13:51:40 -0800

    heroku port

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
 app.use(express.static(__dirname + '/'));
 
 
-app.listen(7000, function () {
+app.listen(process.env.PORT || 7000, function () {
   console.log('dotjamie local on port 7000')
 })
